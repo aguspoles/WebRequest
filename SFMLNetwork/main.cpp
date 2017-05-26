6ff31ca79260cab15946e077423caf253febd4e6
810 <810>
2017-05-26 12:59:16 -0300

    web connection done

--- SFMLNetwork/main.cpp
+++ SFMLNetwork/main.cpp
@@ -2,8 +2,9 @@
 
 int main() {
 	sf::Http http;
-	http.setHost("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22dallas%2C%20tx%22)&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+	http.setHost("http://query.yahooapis.com/");
 	sf::Http::Request request;
+	request.setUri("/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22dallas%2C%20tx%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
 	sf::Http::Response response = http.sendRequest(request);
 	return 0;
 }
