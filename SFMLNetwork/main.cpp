3f20b618606916cac33f5cc67701d86d8959312e
aguspoles <aguspoles@gmail.com>
2017-05-25 23:50:53 -0300

    first commit

---
+++ SFMLNetwork/main.cpp
@@ -0,0 +1,9 @@
+#include "SFML\Network.hpp"
+
+int main() {
+	sf::Http http;
+	http.setHost("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22dallas%2C%20tx%22)&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+	sf::Http::Request request;
+	sf::Http::Response response = http.sendRequest(request);
+	return 0;
+}
